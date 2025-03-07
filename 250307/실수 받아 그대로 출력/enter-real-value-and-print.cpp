1bb5fd412b7f26b77141fcf543c3d66eb88229a4
codie0226 <80187674+codie0226@users.noreply.github.com>
2025-03-07 22:59:29 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250307/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    double n;
+    cin >> n;
+    cout.precision(2);
+    cout << n;
+    return 0;
+}
