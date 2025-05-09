8ec27cdc23d8b3125e111a2dd913d2aea3dd085b
codie0226 <80187674+codie0226@users.noreply.github.com>
2025-05-10 01:14:23 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250510/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cout << fixed;
+    cout.precision(2);
+    cout << a + 1.5;
+    return 0;
+}
